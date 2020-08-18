99b2f9ab062e900c008f946e6041d51837a92c4e
chung <chungwongg@gmail.com>
2020-08-18 14:14:18 -0700

    fixing number

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/', function(req, res){
   }
   console.log("Looking up ticker "+tickerInput.toUpperCase()+"..."); // log to make sure input is correct
   console.log("Searching if price is below "+(tickerPrice)+"..."); // ^^ price 
-  console.log("Sending alerts to "+"'"+numberInput+"'");   // ^^ number
+  console.log("Sending alerts to "+numberInput);   // ^^ number
 
   //api url 
   var url = "https://cloud.iexapis.com/stable/stock/"+(tickerInput)+"/quote/latestPrice?token=pk_a03abb8aebc849bb8a93b6c8f6dbdb71";
@@ -59,16 +59,16 @@ app.post('/', function(req, res){
       console.log(currentPrice);
       if (currentPrice > tickerPrice) {
         console.log("Current price is above input, and will continue searching.")
-        setTimeout(getData, 3000);
+        setTimeout(getData, 300000);
       } else {
         console.log("Current price is below input, will stop searching and alert user at +"+numberInput)
         clearTimeout(getData);
         // twilio text when price is below user input
-      /*  client.messages.create({
-          to: '+'+numberInput,
+        client.messages.create({
+          to: '+14156527111',
           from:'+12057829974',
           body: tickerInput.toUpperCase()+" has fallen below "+tickerPrice+"!"
-        }) */
+        }) 
       }
     } catch (error) {
       console.log(error);
